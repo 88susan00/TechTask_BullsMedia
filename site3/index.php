669f26d42171e4bf8e3dd992d94bc04b73e4638d
Michael <michael.kiselyov@MacBook-Pro-Michael.local>
2026-08-23 12:05:36 +0300

    Fix for healthcheck site

--- site3/index.php
+++ site3/index.php
@@ -1,5 +1,12 @@
 <?php
 
+if ($_SERVER['REQUEST_URI'] === '/healthz') {
+    http_response_code(200);
+    header('Content-Type: text/plain');
+    echo 'OK';
+    exit;
+}
+
 echo "<h1>Site 3 - PHP application</h1>";
 echo "<p>Server time: " . date('Y-m-d H:i:s') . "</p>";
 echo "<p>Hostname: " . gethostname() . "</p>";
